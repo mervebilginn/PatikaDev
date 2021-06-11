9014b7475a83254f1aea88f77881724a42351d7b
mervebilginn <85630863+mervebilginn@users.noreply.github.com>
2021-06-11 22:25:26 +0300

    Add files via upload

---
+++ Patika2.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Patika2 {
+
+    public static void main(String[] args) {
+        
+        double tutar, kdv, kdvTutari, kdvliTutar;
+        
+	Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Ücret tutarını giriniz: ");
+	tutar = scan.nextDouble();
+        
+        kdv = (tutar > 0 && tutar < 1000) ? 0.18 : 0.8;
+	kdvTutari = tutar*kdv;
+    	kdvliTutar = tutar + kdvTutari;
+    	System.out.println("KDV Oranı: " + kdv);
+    	System.out.println("KDV Tutarı: " + kdvTutari);
+    	System.out.println("KDV'li Tutar: " + kdvliTutar); 
+        
+    }
+}
+    
+
