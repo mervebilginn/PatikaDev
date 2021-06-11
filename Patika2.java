56573416f0b489128a9b274b32a6d677e7a34dc9
mervebilginn <85630863+mervebilginn@users.noreply.github.com>
2021-06-11 22:50:41 +0300

    Update Patika2.java

--- Patika2.java
+++ Patika2.java
@@ -11,14 +11,18 @@ public class Patika2 {
         
         System.out.print("Ücret tutarını giriniz: ");
 	tutar = scan.nextDouble();
-        
-        kdv = (tutar > 0 && tutar < 1000) ? 0.18 : 0.8;
-	kdvTutari = tutar*kdv;
-    	kdvliTutar = tutar + kdvTutari;
-    	System.out.println("KDV Oranı: " + kdv);
-    	System.out.println("KDV Tutarı: " + kdvTutari);
-    	System.out.println("KDV'li Tutar: " + kdvliTutar); 
-        
+	  
+	if(tutar>0) {
+            kdv = (tutar > 0 && tutar < 1000) ? 0.18 : 0.8;
+            kdvTutari = tutar*kdv;
+            kdvliTutar = tutar + kdvTutari;
+            System.out.println("KDV Oranı: " + kdv);
+            System.out.println("KDV Tutarı: " + kdvTutari);
+            System.out.println("KDV'li Tutar: " + kdvliTutar); 
+        }
+        else {
+           System.out.println("Geçersiz işlem, programdan çıkılıyor... ");  
+        } 
     }
 }
     
