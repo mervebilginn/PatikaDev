401c23a4d0ab849fba27dc30af901f5611c3d829
mervebilginn <85630863+mervebilginn@users.noreply.github.com>
2021-06-12 16:09:43 +0300

    Update Main.java

--- HavaSicakligiEtkinlik/src/Main.java
+++ HavaSicakligiEtkinlik/src/Main.java
@@ -1,13 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.Scanner;
 
-/**
- *
- * @author merve
- */
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int heat, durum;
+        System.out.print("Hava sıcaklığını giriniz: ");
+        heat = scan.nextInt();
+
+        /*Tekrardan incelenicek*/
+                
+    }
 }
